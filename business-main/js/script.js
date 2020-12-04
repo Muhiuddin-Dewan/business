038b49f6506b0c96ddf2e67eb61233f710d2f419
Muhiuddin-Dewan <75271234+Muhiuddin-Dewan@users.noreply.github.com>
2020-12-04 12:51:16 +0600

    Add files via upload

--- business-main/js/script.js
+++ business-main/js/script.js
@@ -14,6 +14,16 @@ $(function () {
 
 
 });
+//progress-bar js
+$(document).ready(function(){
+	$('#bar1').barfiller({barColor: '#76bbf6',duration: 1000});
+	$('#bar2').barfiller({barColor: '#4f9ee1',duration: 1000});
+	$('#bar3').barfiller({barColor: '#3b89cb',duration: 1000});
+	$('#bar4').barfiller({barColor: '#36b2d4',duration: 1000});
+	$('#bar5').barfiller({barColor: '#1f98b9',duration: 1000});
+	$('#bar6').barfiller({barColor: '#0e7c9a',duration: 1000});	
+});
+//slider js
 $(document).ready(function () {
     $('.slider-nav').slick({
         slidesToShow: 6,
@@ -23,6 +33,7 @@ $(document).ready(function () {
         centerMode: true,
         focusOnSelect: true,
         infinite: true,
-        autoplay:true
+        autoplay: true
     });
 });
+
